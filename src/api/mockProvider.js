0ecb0463f518408b099f7d9d4a8f474f5b9bb56e
ricky90urba <56ztkyb8gh@privaterelay.appleid.com>
2026-05-20 10:38:11 +0200

    Create mockProvider.js

---
+++ src/api/mockProvider.js
@@ -0,0 +1,7 @@
+export function fetchVehicles() {
+  return [
+    { id: 1, type: "scooter", lat: 45.626, lng: 9.035, price: 3.2 },
+    { id: 2, type: "bike", lat: 45.627, lng: 9.037, price: 2.8 },
+    { id: 3, type: "car", lat: 45.625, lng: 9.034, price: 5.5 }
+  ];
+}
